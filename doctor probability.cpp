8f4c97139cf5dead391845e7eca42917b8fbeb0a
Kunal Verma <133907416+mrkunal0430@users.noreply.github.com>
2024-10-24 06:33:06 +0530

    Update doctor probability.cpp
    convert the code to understandable code using vector instead of pointers

--- doctor probability.cpp
+++ doctor probability.cpp
@@ -24,61 +24,59 @@ time is not considered and during that 10min at 10th min he will be in next divi
 */
 
 #include<iostream>
+#include<vector>
 using namespace std;
 
-void docProb(double **graph, int nodes, int time, int curNode, double p, double *answer){
-	if(time <= 0){
-		answer[curNode] += p;
-		return;
-	}
+void docProb(const vector<vector<double>>& graph, int nodes, int time, int curNode, double p, vector<double>& answer) {
+    if (time <= 0) {
+        answer[curNode] += p;  // Add the current probability to the answer for the current node
+        return;
+    }
 
-	for(int i=1; i<=nodes; i++){
-		if(graph[curNode][i] != 0){
-			p *= graph[curNode][i];
-			docProb(graph, nodes, time - 10, i, p, answer);
-			p /= graph[curNode][i];	
-		}
-	}
-
+    for (int i = 0; i < nodes; i++) {  // 0-based indexing for nodes
+        if (graph[curNode][i] != 0) {
+            double nextP = p * graph[curNode][i];  // Multiply the current probability by the transition probability
+            docProb(graph, nodes, time - 10, i, nextP, answer);  // Recursive call for next node
+        }
+    }
 }
 
-int main(){
-	int t;
-	cin >> t;
-	while(t--){
-		int nodes, edges, time;
-		cin >> nodes >> edges >> time;
-		
-		double **arr = new double*[nodes]; 
-		for(int i=1; i<=nodes; i++){
-			arr[i] = new double[nodes];
-			for(int j=1; j<=nodes; j++){
-				arr[i][j] = 0;
-			}
-		}
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int nodes, edges, time;
+        cin >> nodes >> edges >> time;
+        
+        // Initialize the graph with probabilities, 0-based indexing
+        vector<vector<double>> arr(nodes, vector<double>(nodes, 0));
 
-		int from, to;
-		double prob;
-		for(int i=0; i<edges; i++){
-			cin >> from >> to >> prob;
-			arr[from][to] = prob;
-		}
+        int from, to;
+        double prob;
+        for (int i = 0; i < edges; i++) {
+            cin >> from >> to >> prob;
+            arr[from - 1][to - 1] = prob;  // Adjusting for 0-based indexing
+        }
 
-		/* Initalise answer and function call */
-		double answer[nodes] = {0.0};
-		docProb(arr, nodes, time, 1, 1.0, answer);
-		
-		/* Select max Probability node */
-		double finalProb = 0.0;
-		int finalDivison = 0;
-		
-		for(int i=1; i<=nodes; i++){
-			if(answer[i] > finalProb){
-				finalProb = answer[i];
-				finalDivison = i;
-			}
-		}
-		cout << finalDivison << " " << finalProb << "\n";
-	}
-	return 0;
+        // Initialize the answer vector with zeros
+        vector<double> answer(nodes, 0.0);
+
+        // Start from node 0 (assuming 0-based indexing), with initial probability of 1.0
+        docProb(arr, nodes, time, 0, 1.0, answer);
+        
+        // Select the node with the maximum probability
+        double finalProb = 0.0;
+        int finalNode = 0;
+
+        for (int i = 0; i < nodes; i++) {
+            if (answer[i] > finalProb) {
+                finalProb = answer[i];
+                finalNode = i + 1;  // Convert back to 1-based indexing for output
+            }
+        }
+
+        cout << finalNode << " " << finalProb << "\n";
+    }
+    return 0;
 }
+
